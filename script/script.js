21a26df8391de4ddcc08bc9f84fac3b3fb7ca02e
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-10-17 11:27:29 -0300

    dando funcionalidade ao botao iniciar

--- script/script.js
+++ script/script.js
@@ -11,6 +11,7 @@ console.log(btnIniciar);
 
 btnIniciar.addEventListener("click", function(){
     console.log("clicou");
+    iniciar()
 });
 
 btnPause.addEventListener("click", function(){
@@ -21,6 +22,30 @@ btnReiniciar.addEventListener("click", function(){
     console.log("Reiniciou");
 })
 
+function doisDigitos(digito){
+    if(digito <10){
+        return("0" + digito)
+    }else {
+        return(digito)
+    }
+}
 
 
+function cronometoro() {
+    sec++
+    if(sec == 60){
+        min++
+        sec = 0
+        if(min == 60) {
+            hour++
+            min = 0
+        }
+    }
 
+    document.querySelector(".tempo").innerText = doisDigitos(hour)+ ":" + doisDigitos(min) + ":" + doisDigitos(sec)
+};
+
+function iniciar() {
+    cronometoro();
+    intervalo = setInterval(cronometoro, 1000)
+}
