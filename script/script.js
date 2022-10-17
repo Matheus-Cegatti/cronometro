b775e8c320e3d3ab9f175b70a765a077b0ddce92
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-10-17 11:06:28 -0300

    Encotrando os botoes e adicionando o evento de click

--- script/script.js
+++ script/script.js
@@ -3,10 +3,24 @@ var min = 0;
 var hour = 0;
 
 var btnIniciar = document.querySelector(".btn-iniciar");
+var btnPause = document.querySelector(".btn-pausar");
+var btnReiniciar = document.querySelector(".btn-reiniciar");
 
 console.log(btnIniciar);
 
 
 btnIniciar.addEventListener("click", function(){
     console.log("clicou");
+});
+
+btnPause.addEventListener("click", function(){
+    console.log("pausou");
 })
+
+btnReiniciar.addEventListener("click", function(){
+    console.log("Reiniciou");
+})
+
+
+
+
